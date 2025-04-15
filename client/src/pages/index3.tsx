72d3e43b485eecf350edc882f27ed6525a4d0b7e
designerSejinOH <dev@zeroninez.com>
2025-04-15 11:39:48 +0900

    fix

--- client/src/pages/index3.tsx
+++ client/src/pages/index3.tsx
@@ -1,9 +1,9 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import AnimatedModel2 from './AnimatedModel2'
+import AnimatedModel2 from '../components/AnimatedModel2'
 import { useState } from 'react'
 
-function App() {
+export default function Page() {
   const [action, setAction] = useState<'extend' | 'fold'>('extend')
 
   return (
@@ -11,34 +11,43 @@ function App() {
       <Canvas camera={{ position: [0, 0.5, 0.5], fov: 75 }} style={{ width: '100vw', height: '100vh' }}>
         <ambientLight intensity={1.0} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <AnimatedModel2 url="/models/Anatomy/Arm/Arm_Movement.gltf" actionName={action} scale={1.5} position={[0, -0.2, 0]} />
-        <OrbitControls minDistance={0.22} maxDistance={0.4}/>
+        <AnimatedModel2
+          url='/models/Anatomy/Arm/Arm_Movement.gltf'
+          actionName={action}
+          scale={1.5}
+          position={[0, -0.2, 0]}
+        />
+        <OrbitControls minDistance={0.22} maxDistance={0.4} />
       </Canvas>
-      <div style={{ position: 'absolute', top: 150, left: 200, }}>
-        <button onClick={() => setAction('extend')}
+      <div style={{ position: 'absolute', top: 150, left: 200 }}>
+        <button
+          onClick={() => setAction('extend')}
           style={{
             backgroundColor: '#4CAF50',
-            color: 'white',          
+            color: 'white',
             padding: '10px 20px',
             border: 'none',
             borderRadius: '8px',
             cursor: 'pointer',
-            marginBottom: '10px'
-          }}>팔 펴기</button>
+            marginBottom: '10px',
+          }}>
+          팔 펴기
+        </button>
         <br />
-        <button onClick={() => setAction('fold')}
+        <button
+          onClick={() => setAction('fold')}
           style={{
             backgroundColor: '#4CAF50',
-            color: 'white',          
+            color: 'white',
             padding: '10px 20px',
             border: 'none',
             borderRadius: '8px',
             cursor: 'pointer',
-            marginBottom: '10px'
-          }}>팔 접기</button>
+            marginBottom: '10px',
+          }}>
+          팔 접기
+        </button>
       </div>
     </>
   )
 }
-
-export default App
